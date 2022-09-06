e6965fb06397c1d4b262f610ce10ae7e159978bd
federerkristijan <federer.kristijan@gmail.com>
2022-09-06 23:43:50 +0200

    dom done, with button and name input

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -8,48 +8,32 @@ const Contact = () => {
       <div className="form">
         <form>
           <div class="form-group">
-            <label for="exampleFormControlInput1">Email address</label>
+            <label for="name">Example label</label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              placeholder="Your name"
+            />
+          </div>
+          <div class="form-group">
+            <label for="email">Email address</label>
             <input
               type="email"
               class="form-control"
-              id="exampleFormControlInput1"
+              id="email"
               placeholder="name@example.com"
             />
           </div>
-          {/* <div class="form-group">
-            <label for="exampleFormControlSelect1">Example select</label>
-            <select class="form-control" id="exampleFormControlSelect1">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div> */}
-          {/* <div class="form-group">
-            <label for="exampleFormControlSelect2">
-              Example multiple select
-            </label>
-            <select
-              multiple
-              class="form-control"
-              id="exampleFormControlSelect2"
-            >
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div> */}
           <div class="form-group">
-            <label for="exampleFormControlTextarea1">Example textarea</label>
+            <label for="exampleFormControlTextarea1">What</label>
             <textarea
               class="form-control"
               id="exampleFormControlTextarea1"
               rows="3"
             ></textarea>
           </div>
+          <button type="submit" class="btn btn-primary mb-2">Contact me</button>
         </form>
       </div>
     </div>
