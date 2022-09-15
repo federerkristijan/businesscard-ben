fc1b52b2dda917e8bf5d3f2a89c69f05949ffa5b
federerkristijan <federer.kristijan@gmail.com>
2022-09-15 21:39:52 +0200

    sanityClient added, implemented in home.jsx. doesn't render, reaserch more

---
+++ bens-business-card/schemas/home.js
@@ -0,0 +1,22 @@
+export default {
+  title: 'Home',
+  name: 'home',
+  type: 'document',
+  fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string'
+    },
+    {
+      title: 'Text',
+      name: 'text',
+      type: 'text'
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'image'
+    }
+  ]
+}
