c56526a1be6287fa8e18b4e84e881a6fe278d66c
federerkristijan <federer.kristijan@gmail.com>
2022-09-18 12:39:52 +0200

    pages css done. need navbar responsive

--- src/views/Work/Work.jsx
+++ src/views/Work/Work.jsx
@@ -43,10 +43,8 @@ const Work = () => {
                 />
               )}
             </div>
-            <div className="work-title">
-              <h2>{item.title}</h2>
-            </div>
             <div className="work-text">
+              <h1>{item.title}</h1>
               <span>{item.text}</span>
             </div>
             <div className="work-link">
